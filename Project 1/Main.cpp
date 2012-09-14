a9a70341d408032a21959a82416f12a6950d3f24
Pat York <guitarp_11@yahoo.com>
2012-09-14 16:54:55 -0700

    modified:   Project 1/Main.cpp 	Project 1/Mortgage_Test.cpp

--- Project 1/Main.cpp
+++ Project 1/Main.cpp
@@ -18,12 +18,14 @@ using namespace std;
 
 int main(){
 	
-	float version = 1.01f;
-	long loanMoney;
-	
 	Mortgage myMorg;
 	
-	/*cout << "Hello Welcome to the Great Mortgage Calculator  " << (version) << endl <<endl <<endl ;
+	/*float version = 1.01f;
+	long loanMoney;
+	
+	
+	
+	cout << "Hello Welcome to the Great Mortgage Calculator  " << (version) << endl <<endl <<endl ;
 	
 	cout << "Please enter loan amount:  " ;
 	
