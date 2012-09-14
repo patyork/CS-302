3e8d1c185f601c63ed92431ff8b185c7c855b4d7
Roger P Przybyla <przybyla@w-15.ecc.unr.edu>
2012-09-14 14:30:59 -0700

    xx

--- Project 1/Main.cpp
+++ Project 1/Main.cpp
@@ -20,30 +20,52 @@ int main(){
 	
 	float version = 1.01f;
 	long loanMoney;
+	float rate;
+	int payNum;
 	
 	Mortgage myMorg;
 	
-	/*cout << "Hello Welcome to the Great Mortgage Calculator  " << (version) << endl <<endl <<endl ;
+	
+	while(1){
+		
+	cout << "Hello Welcome to the Great Mortgage Calculator  " << (version) << endl <<endl <<endl ;
 	
 	cout << "Please enter loan amount:  " ;
 	
 	cin >> loanMoney ;
 	
-	myMorg.loanAmt(loanMoney);
+	if(loanMoney == 0.0){
+		return 0;
+	}
+	
+	myMorg.setloan(loanMoney);
+	cout << endl <<endl;
+	
+	//paynumber
+	cout << "Please Number Of Payments:  " ;
+	
+	cin >> payNum ;
+	
+	myMorg.setPayNum(payNum);
+	cout << endl <<endl;
+	
+	//rate
+	cout << "Please enter Intrest Rate:  " ;
+	
+	cin >> rate ;
+	
+	myMorg.setRate(rate);
 	
 	
-	cout << endl <<endl;*/
-	
-	myMorg.setloan(10000);
-	myMorg.setPayNum(120);
-	myMorg.setRate(6.75f);
-	cout << myMorg.monthPay() << endl << myMorg.totalAmt() << endl << myMorg.intPay() << endl;
+	cout << endl <<endl;
 	
 	
+	cout << " Monthly Payments: " << myMorg.monthPay() << endl << "Total Amount Paid: "<< myMorg.totalAmt() << endl 
+			<< "Intrest paid: " << myMorg.intPay() << endl << "Intrest rate" <<myMorg.getRate(); <<endl
+			<<"Total Laon Amount" << myMorg.getAmt() <<endl << "Number of Payments: " << myMorg.getPayNum <<endl ;
+			
 	
-	
-	
-	
+	}
 	
 	
 	
