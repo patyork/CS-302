70ed322d36869c1757cd3f30726a033132c01f0e
Roger Paul <rodg@La.(none)>
2012-09-30 19:54:46 -0700

    xx

--- Project2/Product.cpp
+++ Project2/Product.cpp
@@ -2,40 +2,47 @@
 
 	\Implementation for class Product
 
-	\Author:
+	\Author: Roger Przybyla
 
 */
 
 #include "Product.h"
+#include <string.h>
 
 Product::Product()
 {
-
+    number=0;			// Hold the Product Number
+    description[0]='\0';	// Holds the Product Description
+    price=0.0;			// Holds the Product Price
+    taxable=false;	
 }
 
 Product::Product(int num, char* desc, float cost, bool tax)
 {
-
+    number=num;			// Hold the Product Number
+    strcpy(description,desc);	// Holds the Product Description
+    price=cost;			// Holds the Product Price
+    taxable=tax;
 }
 
 int Product::getNumber()
 {
-	return 0;
+	return number;
 }
 
 char* Product::getDescription()
 {
-	return "stub";
+	return &description[0];
 }
 
 float Product::getPrice()
 {
-	return 0.0f;
+	return price;
 }
 
 bool Product::getTax()
 {
-	return false;
+	return taxable;
 }
 
 void Product::addProduct(int num, char* desc, float cost, bool tax)
